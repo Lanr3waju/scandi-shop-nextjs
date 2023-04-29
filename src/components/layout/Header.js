8400f5bb39d5f67038cbee09a8d886e4ecf91711
Abass Olanrewaju <abassolanrewaju111@gmail.com>
2023-04-29 18:50:44 +0100

    feat: create header component

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,6 +1,17 @@
+import store from '../../../data/store.json'
+import Link from 'next/link'
 function Header() {
-    // console.log(store.data.categories.map((category) => category.name))
-    return <div className=' bg-black p-4 mb-4'>Header</div>
+    return (
+        <ul className=' bg-black flex p-4 mb-4 text-white text-xl font-bold'>
+            {store.data.categories.map((category) => {
+                return (
+                    <li key={category.name}>
+                        <Link className='m-4' href='/#'>{category.name}</Link>
+                    </li>
+                )
+            })}
+        </ul>
+    )
 }
 
 export default Header
---
+++ src/pages/api/index.js
@@ -0,0 +1,5 @@
+import store from '../../../data/store.json'
+
+export default function handler(req, res) {
+    res.status(200).json(store)
+}
