f2920dbd36f437a4db61d482de26cade19269e49
Abass Olanrewaju <abassolanrewaju111@gmail.com>
2023-05-07 15:18:43 +0100

    style: format with prettier

--- src/components/Category.js
+++ src/components/Category.js
@@ -9,9 +9,18 @@ export default function Category({ store }) {
   return (
     <ul className="flex flex-wrap justify-start gap-20 pb-5 min-h-screen">
       {store?.products.map(({ id, gallery, name, prices, inStock }) => (
-        <li key={id} className="w-80 h-96 shadow-sm rounded-lg p-6 shadow-hoverBackground hover:shadow-overlay hover:shadow-lg transition-shadow relative">
-          <span className={`text-3xl font-medium text-overlay absolute top-1/4 left-[18%] -rotate-45 opacity-100 z-20 ${inStock && 'hidden'}`}>Out of stock</span>
-          <Link className={`${!inStock && 'opacity-40'}`} href={`/${id}`}>
+        <li
+          key={id}
+          className="w-80 h-96 shadow-sm rounded-lg p-6 shadow-hoverBackground hover:shadow-overlay hover:shadow-lg transition-shadow relative"
+        >
+          <span
+            className={`text-3xl font-medium text-overlay absolute top-1/4 left-[18%] -rotate-45 opacity-100 z-20 ${
+              inStock && "hidden"
+            }`}
+          >
+            Out of stock
+          </span>
+          <Link className={`${!inStock && "opacity-40"}`} href={`/${id}`}>
             <Image
               className="w-full h-[88%] object-contain border-b-2 border-primary pb-2 -z-10 relative"
               src={gallery[0]}
@@ -20,7 +29,8 @@ export default function Category({ store }) {
               placeholder={true}
               alt={name}
             />
-            <h2 className="opacity-80 my-3">{name}
+            <h2 className="opacity-80 my-3">
+              {name}
               {prices.map((price) => {
                 if (price.currency.symbol === currency) {
                   return (
--- src/pages/[description]/index.js
+++ src/pages/[description]/index.js
@@ -1,14 +1,16 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
-import store from '../../../data/store.json';
+import store from "../../../data/store.json";
 import Image from "next/image";
 import { useState, useEffect } from "react";
 import ImageMagnifier from "@/components/Magnifier";
 
 export default function DescriptionPage() {
   const router = useRouter();
-  const product = store.data.categories[0].products.find((product) => product.id === router.query.description);
-  const [activeImage, setActiveImage] = useState('/placeholder.png');
+  const product = store.data.categories[0].products.find(
+    (product) => product.id === router.query.description
+  );
+  const [activeImage, setActiveImage] = useState("/placeholder.png");
 
   useEffect(() => {
     product && setActiveImage(product?.gallery[0]);
@@ -22,15 +24,21 @@ export default function DescriptionPage() {
       <main className="flex p-14">
         <section className="w-1/4">
           <ul className="w-full">
-            {
-              product?.gallery.map((image) => (
-                <li key={image}>
-                  <button className="mb-4" onClick={() => setActiveImage(image)}>
-                    <Image placeholder="blur" blurDataURL="/small-placeholder.png" className="w-24 h-24 object-contain" src={image} alt='product' width={96} height={96} />
-                  </button>
-                </li>
-              ))
-            }
+            {product?.gallery.map((image) => (
+              <li key={image}>
+                <button className="mb-4" onClick={() => setActiveImage(image)}>
+                  <Image
+                    placeholder="blur"
+                    blurDataURL="/small-placeholder.png"
+                    className="w-24 h-24 object-contain"
+                    src={image}
+                    alt="product"
+                    width={96}
+                    height={96}
+                  />
+                </button>
+              </li>
+            ))}
           </ul>
         </section>
         <section className="w-full">
