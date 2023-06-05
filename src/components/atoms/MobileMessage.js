c16cfaca0a59213d6a8e70a631c0f6b1e562d108
Abass Olanrewaju <abassolanrewaju111@gmail.com>
2023-06-05 08:23:34 +0100

    fix: update mobile screen text

--- src/components/atoms/MobileMessage.js
+++ src/components/atoms/MobileMessage.js
@@ -1,10 +1,10 @@
 const MobileMessage = () => {
   return (
     <div>
-      <h2 className="text-error text-2xl text-center">
+          <h2 className="text-error text-2xl text-center m-3">
         Please view this application on a larger screen.
       </h2>
-      <p>Support for mobile screens coming soon! 🙃</p>
+          <p className="text-center">Support for mobile screens coming soon! 🙃</p>
     </div>
   )
 }
